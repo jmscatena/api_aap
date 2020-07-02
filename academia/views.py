9fa16219c37cc5e06f8d4390212aaa2d19c3ad35
Jean Miler Scatena <jean.scatena@fatec.sp.gov.br>
2020-07-02 14:00:46 +0000

    Ultima Atualizacao do Repl.it

--- academia/views.py
+++ academia/views.py
@@ -27,23 +27,23 @@ def novo_usuario(request):
                     gestor = form.cleaned_data.get('gestor'),)
       command = format_html('<div class="alert alert-success popup"><h4 style="text-align:center;">Cadastro de Cliente</h4><p style="text-align:center;">Realizado com Sucesso!</p></div>')
       context = {'form': form, 'alert': command }
-      return render(request, 'usuarios.html', context)
+      return render(request, 'cadastros/usuarios.html', context)
   else:
     form = UsuariosForm()  
-  return render(request, 'usuarios.html', {'form':form})
+  return render(request, 'cadastros/usuarios.html', {'form':form})
 
 def lista_usuarios(request):
-  alunos = Usuarios.objects.all()
-  return render(request, 'consultas/usuarios.html', {'alunos':alunos})
+  usuarios = Usuarios.objects.all()#Select *
+  return render(request, 'consultas/usuarios.html', {'usuarios':usuarios})
 
 def lista_alunos(request):
-  alunos = Usuarios.objects.filter(aluno__iexact='TRUE')
+  alunos = Usuarios.objects.filter(aluno__iexact=1)# Select Where aluno=True
   return render(request, 'consultas/alunos.html', {'alunos':alunos})
 
 def lista_professores(request):
-  professores = Usuarios.objects.filter(professor__iexact='TRUE')
+  professores = Usuarios.objects.filter(professor__iexact=1)
   return render(request, 'consultas/professores.html', {'professor':professores})
 
 def lista_gestores(request):
-  gestores = Usuarios.objects.filter(gestor__iexact='TRUE')
+  gestores = Usuarios.objects.filter(gestor__iexact=1)
   return render(request, 'consultas/gestores.html', {'gestores':gestores})
