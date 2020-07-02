df935cc8a2a65b9ee4e9baf01572b5c52f9839d8
Jean Miler Scatena <43283851+jmscatena@users.noreply.github.com>
2020-07-02 11:06:33 -0300

    Arquivos externos alunos - css e js

---
+++ templates/JS/tela_inicio.js
@@ -0,0 +1,10 @@
+/*function changeLogo() {
+            var image = document.getElementById('myImage');
+            if (image.src.match("white")) {
+                image.src = "logo_altere_dark.jpg";
+            } 
+            else { 
+                    image.src = "logo_altere_white.jpg"; 
+                }
+        }
+*/
